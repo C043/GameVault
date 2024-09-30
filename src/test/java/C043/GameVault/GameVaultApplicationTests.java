43bb9b1f33a4de0e7168ab7faaab58e835c92dfa
C043 <mariofragnitoph@gmail.com>
2024-09-30 18:04:31 +0200

    Initial commit

---
+++ src/test/java/C043/GameVault/GameVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package C043.GameVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
