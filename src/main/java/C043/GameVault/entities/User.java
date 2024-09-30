126305389271b55c8e34e291eecd6afd56a83207
C043 <mariofragnitoph@gmail.com>
2024-09-30 18:20:42 +0200

    Adds User entity

---
+++ src/main/java/C043/GameVault/entities/User.java
@@ -0,0 +1,33 @@
+package C043.GameVault.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private int id;
+
+    private String username;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private String avatar;
+    private String cover;
+
+    public User(String username, String name, String surname, String email, String password) {
+        this.username = username;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+    }
+}
