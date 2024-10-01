6b695de402b387782efba8af2ceb520d5884eb46
C043 <mariofragnitoph@gmail.com>
2024-10-02 00:29:16 +0200

    Adds GamesController

---
+++ src/main/java/C043/GameVault/controllers/GamesController.java
@@ -0,0 +1,25 @@
+package C043.GameVault.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/games")
+public class GamesController {
+    private final String url = "https://api.rawg.io/api/";
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${rawg.key}")
+    private String key;
+
+    @GetMapping
+    public ResponseEntity<String> getGames() {
+        return restTemplate.getForEntity(this.url + "/games?key=" + key, String.class);
+    }
+}
