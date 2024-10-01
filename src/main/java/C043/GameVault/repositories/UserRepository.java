14b1aca29bf1388a341cc6bdac2158a9bbcb3bd2
Mario Fragnito <mariofragnitoph@gmail.com>
2024-10-01 17:37:58 +0200

    Adds UserRepository

---
+++ src/main/java/C043/GameVault/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package C043.GameVault.repositories;
+
+import C043.GameVault.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
