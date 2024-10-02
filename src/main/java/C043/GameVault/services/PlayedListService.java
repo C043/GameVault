eb52e44f508c74be35de89f1582d00b09a42a360
Mario Fragnito <mariofragnitoph@gmail.com>
2024-10-02 19:17:32 +0200

    Adds playedList automatic handling

--- src/main/java/C043/GameVault/services/PlayedListService.java
+++ src/main/java/C043/GameVault/services/PlayedListService.java
@@ -1,12 +1,16 @@
 package C043.GameVault.services;
 
+import C043.GameVault.entities.BackLog;
 import C043.GameVault.entities.PlayedList;
+import C043.GameVault.entities.PlayingList;
 import C043.GameVault.entities.User;
 import C043.GameVault.exceptions.BadRequestException;
 import C043.GameVault.exceptions.NotFoundException;
 import C043.GameVault.exceptions.UnauthorizedException;
 import C043.GameVault.payloads.GameDTO;
+import C043.GameVault.repositories.BackLogRepository;
 import C043.GameVault.repositories.PlayedListRepository;
+import C043.GameVault.repositories.PlayingListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,9 +21,25 @@ public class PlayedListService {
     @Autowired
     private PlayedListRepository playedListRepository;
 
+    @Autowired
+    private BackLogRepository backLogRepository;
+
+    @Autowired
+    private BackLogService backLogService;
+
+    @Autowired
+    private PlayingListRepository playingListRepository;
+
+    @Autowired
+    private PlayingService playingService;
+
     public PlayedList postPlaying(GameDTO body, User user) {
         PlayedList found = this.playedListRepository.findByGameIdAndUser(body.gameId(), user);
         if (found != null) throw new BadRequestException("Game already in list");
+        BackLog foundBackLog = this.backLogRepository.findByGameIdAndUser(body.gameId(), user);
+        if (foundBackLog != null) this.backLogService.deleteBackLog(user, foundBackLog.getGameId());
+        PlayingList foundPlaying = this.playingListRepository.findByGameIdAndUser(body.gameId(), user);
+        if (foundPlaying != null) this.playingService.deletePlayingList(user, foundPlaying.getGameId());
         PlayedList newPlaying = new PlayedList(body.gameId(), user);
         return this.playedListRepository.save(newPlaying);
     }
