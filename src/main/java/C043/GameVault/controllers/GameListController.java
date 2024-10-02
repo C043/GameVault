5e48f7a8bad7578fbb94570ceec33ad6ab1d8aac
Mario Fragnito <mariofragnitoph@gmail.com>
2024-10-02 18:02:56 +0200

    Adds playingList endpoints

--- src/main/java/C043/GameVault/controllers/GameListController.java
+++ src/main/java/C043/GameVault/controllers/GameListController.java
@@ -1,11 +1,13 @@
 package C043.GameVault.controllers;
 
 import C043.GameVault.entities.BackLog;
+import C043.GameVault.entities.PlayingList;
 import C043.GameVault.entities.User;
 import C043.GameVault.payloads.GameDTO;
 import C043.GameVault.payloads.RespDTO;
 import C043.GameVault.security.Validation;
 import C043.GameVault.services.BackLogService;
+import C043.GameVault.services.PlayingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -23,11 +25,14 @@ public class GameListController {
     private BackLogService backLogService;
 
     @Autowired
+    private PlayingService playingService;
+
+    @Autowired
     private Validation validation;
 
     @PostMapping("/backlog")
     @ResponseStatus(HttpStatus.CREATED)
-    public RespDTO postListEntry(@AuthenticationPrincipal User user, @RequestBody @Validated GameDTO body, BindingResult validation) {
+    public RespDTO postBacklogEntry(@AuthenticationPrincipal User user, @RequestBody @Validated GameDTO body, BindingResult validation) {
         this.validation.validate(validation);
         BackLog newBacklog = this.backLogService.postBackLog(body, user);
         return new RespDTO(newBacklog.getId());
@@ -42,5 +47,24 @@ public class GameListController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteBackLog(@AuthenticationPrincipal User user, @PathVariable int backlogId) {
         this.backLogService.deleteBackLog(user, backlogId);
+    }
+
+    @PostMapping("/playing")
+    @ResponseStatus(HttpStatus.CREATED)
+    public RespDTO postPlayingEntry(@AuthenticationPrincipal User user, @RequestBody @Validated GameDTO body, BindingResult validation) {
+        this.validation.validate(validation);
+        PlayingList newPlayingList = this.playingService.postPlaying(body, user);
+        return new RespDTO(newPlayingList.getId());
+    }
+
+    @GetMapping("/playing")
+    public List<PlayingList> getPlayingList(@AuthenticationPrincipal User user) {
+        return this.playingService.getPlayingListByUser(user);
+    }
+
+    @DeleteMapping("/playing/{playingId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletePlaying(@AuthenticationPrincipal User user, @PathVariable int playingId) {
+        this.playingService.deletePlayingList(user, playingId);
     }
 }
---
+++ src/main/java/C043/GameVault/services/PlayingService.java
@@ -0,0 +1,40 @@
+package C043.GameVault.services;
+
+import C043.GameVault.entities.PlayingList;
+import C043.GameVault.entities.User;
+import C043.GameVault.exceptions.BadRequestException;
+import C043.GameVault.exceptions.NotFoundException;
+import C043.GameVault.exceptions.UnauthorizedException;
+import C043.GameVault.payloads.GameDTO;
+import C043.GameVault.repositories.PlayingListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PlayingService {
+    @Autowired
+    private PlayingListRepository playingListRepository;
+
+    public PlayingList postPlaying(GameDTO body, User user) {
+        PlayingList found = this.playingListRepository.findByGameId(body.gameId());
+        if (found != null) throw new BadRequestException("Game already in list");
+        PlayingList newPlaying = new PlayingList(body.gameId(), user);
+        return this.playingListRepository.save(newPlaying);
+    }
+
+    public List<PlayingList> getPlayingListByUser(User user) {
+        return this.playingListRepository.findAllByUser(user);
+    }
+
+    public PlayingList getPlayingListById(int id) {
+        return this.playingListRepository.findById(id).orElseThrow(() -> new NotFoundException("Game not found"));
+    }
+
+    public void deletePlayingList(User user, int id) {
+        PlayingList found = this.getPlayingListById(id);
+        if (found.getUser().getId() != user.getId()) throw new UnauthorizedException("You don't have permissions to delete this game");
+        this.playingListRepository.delete(found);
+    }
+}
