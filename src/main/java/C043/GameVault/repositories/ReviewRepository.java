4b0bb432ed7386a44ae2ad2a9ff123abaccb8fb7
C043 <mariofragnitoph@gmail.com>
2024-10-17 11:47:19 +0200

    Adds review repository

---
+++ src/main/java/C043/GameVault/repositories/ReviewRepository.java
@@ -0,0 +1,9 @@
+package C043.GameVault.repositories;
+
+import C043.GameVault.entities.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+}
