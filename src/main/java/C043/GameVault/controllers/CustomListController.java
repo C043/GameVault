f7f18c2878ca49f87a63431c085661501c2f307d
C043 <mariofragnitoph@gmail.com>
2024-10-18 11:25:37 +0200

    Adds post custom list endpoint

---
+++ src/main/java/C043/GameVault/controllers/CustomListController.java
@@ -0,0 +1,35 @@
+package C043.GameVault.controllers;
+
+import C043.GameVault.entities.CustomList;
+import C043.GameVault.entities.User;
+import C043.GameVault.payloads.CustomListDTO;
+import C043.GameVault.payloads.RespDTO;
+import C043.GameVault.security.Validation;
+import C043.GameVault.services.CustomListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/customLists")
+public class CustomListController {
+    @Autowired
+    private CustomListService customListService;
+
+    @Autowired
+    private Validation validation;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public RespDTO postCustomList(@AuthenticationPrincipal User user,
+                                  @RequestBody @Validated CustomListDTO body,
+                                  BindingResult validation) {
+        this.validation.validate(validation);
+        CustomList newCustomList = this.customListService.postCustomList(user
+                , body);
+        return new RespDTO(newCustomList.getId());
+    }
+}
---
+++ src/main/java/C043/GameVault/services/CustomListService.java
@@ -0,0 +1,19 @@
+package C043.GameVault.services;
+
+import C043.GameVault.entities.CustomList;
+import C043.GameVault.entities.User;
+import C043.GameVault.payloads.CustomListDTO;
+import C043.GameVault.repositories.CustomListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomListService {
+    @Autowired
+    private CustomListRepository customListRepository;
+
+    public CustomList postCustomList(User user, CustomListDTO body) {
+        return this.customListRepository.save(new CustomList(body.title(),
+                user));
+    }
+}
